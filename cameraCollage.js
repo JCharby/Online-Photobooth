3a39848a8e0092b806e34783b3f0b72de8cc2e7b
John Charboneau <john23Charboneau@gmail.com>
2020-04-22 14:49:58 -0500

    mixing area

--- cameraCollage.js
+++ cameraCollage.js
@@ -20,55 +20,55 @@ let canvasMixArea = null
 let canvas = null
 
 
-function startUp(){
-cameraContentarea = document.querySelector('#cameraContentarea')
-canvasMixArea = document.querySelector('#canvasMixArea')
-cameraContentarea.className = 'displayBlock'
-canvasMixArea.className = 'displayNone'
+function startUp() {
+  cameraContentarea = document.querySelector('#cameraContentarea')
+  canvasMixArea = document.querySelector('#canvasMixArea')
+  cameraContentarea.className = 'displayBlock'
+  canvasMixArea.className = 'displayNone'
 
-video = document.querySelector('#video')
-canvasPhoto = document.querySelector('#canvasPhoto')
-photo = document.querySelector('#photo')
-startButton = document.querySelector('#startButton')
-lips = document.querySelector('#lips')
-glasses = document.querySelector('#glasses')
-horns = document.querySelector('#horns')
-db = document.querySelector('#downloadButton')
-returnButton = document.querySelector('#returnButton')
-startMixingButton = document.querySelector('#startMixingButton')
+  video = document.querySelector('#video')
+  canvasPhoto = document.querySelector('#canvasPhoto')
+  photo = document.querySelector('#photo')
+  startButton = document.querySelector('#startButton')
+  lips = document.querySelector('#lips')
+  glasses = document.querySelector('#glasses')
+  horns = document.querySelector('#horns')
+  db = document.querySelector('#downloadButton')
+  returnButton = document.querySelector('#returnButton')
+  startMixingButton = document.querySelector('#startMixingButton')
 
-canvas = new fabric.Canvas('c')
+  canvas = new fabric.Canvas('c')
 
-startButton.addEventListener('click', takePicture, false)
-startMixingButton.addEventListener('click', startMixingFunction, false)
-downloadButton.addEventListener('click', saveImage, false)
-returnButton.addEventListener('click', returnFunction, false)
-lips.addEventListener('click', placeImage, false)
-glasses.addEventListener('click', placeImage, false)
-horns.addEventListener('click', placeImage, false)
+  startButton.addEventListener('click', takePicture, false)
+  startMixingButton.addEventListener('click', startMixingFunction, false)
+  downloadButton.addEventListener('click', saveImage, false)
+  returnButton.addEventListener('click', returnFunction, false)
+  lips.addEventListener('click', placeImage, false)
+  glasses.addEventListener('click', placeImage, false)
+  horns.addEventListener('click', placeImage, false)
 
-video.addEventListener('click', function(){
-  video.play()
-}, false)
-
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: false
-  })
-  .then(function(stream){
-    video.srcObject = stream
+  video.addEventListener('click', function() {
     video.play()
-  })
-  .catch(function(err){
-    console.log("An error has happened: " + err)
-  })
+  }, false)
 
-  video.addEventListener('canplay', function(e){
-    if (!streaming){
+  navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false
+    })
+    .then(function(stream) {
+      video.srcObject = stream
+      video.play()
+    })
+    .catch(function(err) {
+      console.log("An error has happened: " + err)
+    })
+
+  video.addEventListener('canplay', function(e) {
+    if (!streaming) {
       hegiht = video.videoHeight / (video.videoWidth / width)
 
-      if (isNaN(height)){
-        height - width / (4/3)
+      if (isNaN(height)) {
+        height - width / (4 / 3)
       }
       video.setAttribute('width', width)
       video.setAttribute('height', height)
@@ -80,50 +80,67 @@ navigator.mediaDevices.getUserMedia({
   }, false)
 
 
-clearPhoto()
+  clearPhoto()
 
 
-}//end startUp
+} //end startUp
 
-function clearPhoto(){
-let ctx = canvasPhoto.getContext('2d')
-ctx.fillStyle = "#fa00ff"
-ctx.fillRect(0,0,canvasPhoto.width, canvasPhoto.height)
-let data = canvasPhoto.toDataURL('image/png')
-photo.setAttribute('src', data)
-}//end clearPhoto
-
-
-function takePicture(){
-let ctx = canvasPhoto.getContext('2d')
-if (width && height){
-  canvasPhoto.width = width
-  canvasPhoto.height = height
-  ctx.drawImage(video,0,0,width,height)
-
+function clearPhoto() {
+  let ctx = canvasPhoto.getContext('2d')
+  ctx.fillStyle = "#fa00ff"
+  ctx.fillRect(0, 0, canvasPhoto.width, canvasPhoto.height)
   let data = canvasPhoto.toDataURL('image/png')
   photo.setAttribute('src', data)
-} else {
-  clearPhoto()
-}
-
-}//end takePicture
-function startMixingFunction(){
+} //end clearPhoto
 
 
-}//end startMixingFuction
-function saveImage(){
+function takePicture() {
+  let ctx = canvasPhoto.getContext('2d')
+  if (width && height) {
+    canvasPhoto.width = width
+    canvasPhoto.height = height
+    ctx.drawImage(video, 0, 0, width, height)
+
+    let data = canvasPhoto.toDataURL('image/png')
+    photo.setAttribute('src', data)
+  } else {
+    clearPhoto()
+  }
+
+} //end takePicture
+
+/////////////////end camera
+/////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+/////////////
+////////////////////////////////////////////////////////////////
 
 
-}//end saveImage
-function returnFunction(){
+function startMixingFunction() {
+  cameraContentarea.className = 'displayNone'
+  canvasMixArea.classname = 'displayBlock'
+
+  fabric.Image.fromURL('images/horns.png', function(hImg) {
+    hImg.set({
+      left: Math.random() = 600 + 100,
+      top: Math.random() = 400 + 100,
+    })
+    canvas.add(hImg)
+  })
+} //end startMixingFuction
+
+function saveImage() {
 
 
-}//end returnFuction
-function placeImage(){
+} //end saveImage
+function returnFunction() {
 
 
-}//end placeImage
+} //end returnFuction
+function placeImage() {
+
+
+} //end placeImage
 
 
 
